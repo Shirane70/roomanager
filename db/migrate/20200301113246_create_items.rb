46e8fc49661879bd7dec3e1a9c32d713389ed544
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-01 20:38:56 +0900

    table5making

--- db/migrate/20200301113246_create_items.rb
+++ db/migrate/20200301113246_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.integer :cost
+      t.date :date
+      t.text :memo
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_112533) do
+ActiveRecord::Schema.define(version: 2020_03_01_113246) do
 
   create_table "furnitures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_03_01_112533) do
   create_table "houses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "type"
+    t.integer "cost"
+    t.date "date"
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.integer "cost"
     t.date "date"
     t.text "memo"
